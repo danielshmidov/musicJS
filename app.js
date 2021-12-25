e7e8d08edea5c033df16e715143c58821eb4ae55
Daniel Shmidov <daniel.shmidov@gmail.com>
2021-12-25 00:19:59 -0800

    Feat: Event Lisener for box, fix: query select
    For the pads I have implemented an event listener that calls a function that changes the colour when of the pad when it is clicked and also added a dynamic BPM functionality

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 class Drunkit {
   constructor() {
-    this.pad = document.querySelector(".pad");
+    this.pads = document.querySelectorAll(".pad");
+    this.playButton = document.querySelector(".play");
     this.KickAudio = document.querySelector(".kick-sound");
     this.SnareAudio = document.querySelector(".snare-sound");
     this.HihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
   }
+
+  activePad() {
+    this.classList.toggle("active"); //enableing the active CSS configure to the pad
+  }
+
   repeat() {
     let step = this.index % 8;
-    const activeBars = document.querySelectorAll(`.b`+step);   //Selecting the active bar
-    this.index++; //Incrementing the index 
+    const activeBars = document.querySelectorAll(`.b` + step); //Selecting the active bar
+    console.log(step);
+    this.index++; //Incrementing the index
   }
   start() {
+    const interval = (60 / this.bpm) * 1000; // a representation of how quickly the interval is being reached
     setInterval(() => {
       this.repeat();
-    }, 1000);
+    }, interval);
   }
 }
 
 const drumkit = new Drunkit();
 
-drumkit.start(); //starting the drumkit
+drumkit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumkit.activePad);
+});
+
+drumkit.playButton.addEventListener("click", (e) => {
+  drumkit.start();
+});
